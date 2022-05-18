e375c853b1ee4af04fbc2e573fd61faba4b0462c
Gustavo <gucostapereira777@gmail.com>
2022-05-17 23:47:03 -0300

    adicionar perfil

--- home/home.js
+++ home/home.js
@@ -3,3 +3,12 @@ const usuarioLogado = JSON.parse(localStorage.getItem('usuarioLogado'));
 if (!usuarioLogado) {
     window.location.assign('../login.html')
 }
+
+window.onload = () => {
+    document.getElementById("usuarioNome").innerHTML = usuarioLogado.usuarioCad
+}
+
+function sair () {
+    localStorage.removeItem('usuarioLogado');
+    window.location.assign('../login.html')
+}
