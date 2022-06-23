904a63734f8ce52dd1eaed99f4e7ceb1e0d96905
Gustavo Costa <gucostapereira777@gmail.com>
2022-06-23 19:27:54 -0300

    adicionar função de marretar usuários

--- login/login.js
+++ login/login.js
@@ -1,5 +1,60 @@
-function entrar() {
+const marretarUsuarios = () => {
+    const listaUser = JSON.parse(localStorage.getItem('listaUser') || '[]')
 
+    if (listaUser.length === 0) {
+        const usuarios = [
+            {
+                id: 1,
+                nomeCad: 'Maria',
+                emailCad: 'maria@maria.com',
+                senhaCad: '123456',
+                usuarioCad: '@maria',
+                telefoneCad: '31967389293',
+            },
+            {
+                id: 2,
+                nomeCad: 'João',
+                emailCad: 'joao@joao.com',
+                senhaCad: '654321',
+                usuarioCad: '@joao',
+                telefoneCad: '3173520977',
+            }
+        ]
+
+        const pets = [
+            {
+                id: 1,
+                nomeAnimalCad: 'Bilu',
+                especieCad: 'Cachorro',
+                idadeCad: 8,
+                descricaoCad: 'Bilú é muito dócil e amoroso, gosta de brincar e fazer estripulias... Yuuppiiii',
+                idDono: 1
+            },
+            {
+                id: 2,
+                nomeAnimalCad: 'Alonzo',
+                especieCad: 'Gato',
+                idadeCad: 2,
+                descricaoCad: 'Alonzo gosta de ser tratado com respeito, prefere não dar muita bola para estranhos e dorme muito.',
+                idDono: 1
+            },
+            {
+                id: 3,
+                nomeAnimalCad: 'Toby (O Maguire)',
+                especieCad: 'Cachorro',
+                idadeCad: 1,
+                descricaoCad: 'Toby é fanático pelo universo Marvel, está precisando de um Sr. Tony Stark em sua vida!',
+                idDono: 2
+            }
+        ]
+
+        localStorage.setItem('listaUser', JSON.stringify(usuarios));
+        localStorage.setItem('listaAnimal', JSON.stringify(pets));
+        console.log('usuários marretados com sucesso!')
+    }
+}
+
+function entrar() {
 
     const email = document.querySelector('#email').value
     const password = document.querySelector('#password').value
@@ -11,11 +66,13 @@ function entrar() {
     if (usuarioLogado.length === 0) {
         alert("Insira os dados nos campos corretamente!")
         return;
-    } 
+    }
 
     localStorage.setItem('usuarioLogado', JSON.stringify(usuarioLogado[0]));
-   
+
     window.location.assign('/home/home.html')
 
     return false
 }
+
+marretarUsuarios()
